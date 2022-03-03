6abde1b83df3d6df26e990298619407160c1d8d7
kimjuneseo <june3929213512@gmail.com>
2022-03-03 19:54:02 +0900

    1일차

--- app/index.js
+++ app/index.js
@@ -5,29 +5,34 @@ const ctx = canvas.getContext('2d');
 let tool = "pen";
 
 let drwaChk = false;
+let drawRectX; 
+let drawRectY;
 const mouseDownListener = (e) => {
-    ctx.beginPath();
     
-    if(tool === "pen" || tool === "eraser"){
-        drwaChk = true;
-        ctx.strokeStyle = $("#color").value
-        moveTo(e.clientX, e.clientY);
-        return;
+    ctx.beginPath();
+    drwaChk = true;
+    ctx.strokeStyle = $("#color").value;
+    ctx.moveTo(e.clientX - 10, e.clientY - 10);
+    if(tool === "rect"){
+        drawRectX = e.clientX;
+        drawRectY = e.clientY;
+        ctx.clearRect(drawRectX, drawRectY,e.clientX - drawRectX, e.clientY - drawRectY);
     }
 
-    if( "rect"){
-
-    }
 };  
       
 const mouseMoveListener = (e) => {
     if(drwaChk){
-        if(e.clientX >= window.innerWidth || e.clientX <= 0 || e.clientY >= window.innerHeight- 5 || e.clientY <= 0 ) drwaChk = false;
-        ctx.lineTo(e.clientX, e.clientY)
+        if(e.clientX >= window.innerWidth - 5 || e.clientX <= 0 || e.clientY >= window.innerHeight- 5 || e.clientY <= 0 ) drwaChk = false;
         if(tool === 'eraser' ){
             ctx.globalCompositeOperation = "source-over";
             ctx.strokeStyle = "#fff";
         };
+        if( tool === "rect"){
+            ctx.fillRect(drawRectX, drawRectY, e.clientX - drawRectX, e.clientY - drawRectY);
+            return;
+        }
+        ctx.lineTo(e.clientX - 10, e.clientY - 10);
         ctx.stroke();
     }
 };
@@ -50,8 +55,9 @@ const init = () => {
     canvas.width = window.innerWidth; 
     canvas.height = window.innerHeight; 
     ctx.lineWidth = 10;
+    ctx.lineCap = "round"
+    ctx.lineJoin = "round"
 };
-
 init();
 
 
